415e332577028435df6ed66325d99bddf1b5cb04
Titus Wormer <tituswormer@gmail.com>
2022-11-06 15:45:32 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {brill} from './index.js'
 
-test('brill', function (t) {
-  t.deepEqual(
+test('brill', function () {
+  assert.deepEqual(
     brill.that,
     ['IN', 'DT', 'NN', 'RB', 'RP', 'UH', 'WP', 'VBP', 'WDT'],
     'should contain words'
   )
-
-  t.end()
 })
